1dbcf25e9a4cc9f0415497e29b661f2bdc92624c
sebastian <sebascarreram@hotmail.com>
2025-06-11 23:37:39 -0500

    Added GET user by ID

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const mongoose = require("mongoose")
 
-// GET ALL USERS
+// GET ALL USERS => GET Method
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -16,7 +18,31 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-// POST USER
+// GET USER BY ID => GET Method
+
+exports.getUser = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  
+  // ✅ Validar que el ID sea un ObjectId válido
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(new AppError(`Invalid ID format: ${id}`, 400));
+  }
+  const user = await User.findById(id);
+
+  if (!user) {
+    return next(
+      new AppError(`No USER found with that ID: ${req.params.id}`, 404)
+    );
+  }
+
+  // Status 200 is successed
+  res.status(200).json({
+    status: "Success found",
+    data: { user },
+  });
+});
+
+// POST USER => POST Method
 exports.createUser = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
